793bc826e5a32406b6baddbb7fa65ccc4e6c1ea5
Bidhan012-ops <berabidhan012@gmail.com>
2026-07-11 16:32:57 +0530

    modify middleware

--- middleware.ts
+++ middleware.ts
@@ -22,7 +22,7 @@ export async function middleware(request: NextRequest) {
 
   if (token && (pathname.startsWith("/login") || pathname.startsWith("/signin") || pathname.startsWith("/signup"))) {
     console.log("Redirecting to dashboard from middleware");
-    return NextResponse.redirect(new URL("/dashboard", request.url));
+    return NextResponse.redirect(new URL("/deshboard", request.url));
   }
   if (!token && (pathname.startsWith("/deshboard") || pathname.startsWith("/landing"))) {
     return NextResponse.redirect(new URL("/signin", request.url));
